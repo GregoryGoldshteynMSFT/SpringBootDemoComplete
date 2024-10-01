a2bfb98bab4f90abed5343d9683bff90357afddd
Gregory Goldshteyn <grgoldsh@microsoft.com>
2024-10-01 19:12:58 -0400

    Remove unused test. Fix extant test

--- src/test/java/com/microsoft/azure/appservice/demo/app/AppApplicationTests.java
+++ src/test/java/com/microsoft/azure/appservice/demo/app/AppApplicationTests.java
@@ -3,7 +3,7 @@ package com.microsoft.azure.appservice.demo.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AppApplication.class)
 class AppApplicationTests {
 
 	@Test
