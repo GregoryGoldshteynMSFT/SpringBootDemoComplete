2e9b46fd7b9951dd35b5ef9284a4912b7f0f4db4
Gregory Goldshteyn <grgoldsh@microsoft.com>
2024-10-01 18:59:11 -0400

    Initial commit from Spring Init

---
+++ src/test/java/com/microsoft/azure/appservice/grgoldsh/demo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.azure.appservice.grgoldsh.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
